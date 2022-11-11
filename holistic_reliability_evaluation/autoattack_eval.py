e55e1fb8630237f81361adb6f2268580260c5190
David Karamardian <dk11@stanford.edu>
2022-11-11 13:23:01 -0800

    commit

--- holistic_reliability_evaluation/autoattack_eval.py
+++ holistic_reliability_evaluation/autoattack_eval.py
@@ -1,5 +1,12 @@
 exec(open('holistic_reliability_evaluation/evaluation.py').read())
 
+print(input)
+print(y)
+
+input = input.cuda()
+y = y.cuda()
+model = model.cuda()
+
 from autoattack import AutoAttack
 adversary = AutoAttack(model, norm='Linf', eps=8/255, version='standard')
 adversary.run_standard_evaluation(input, y)
