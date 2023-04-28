c0ba6518881e35c8809c02a66fd51f9ee507b1a9
Romeo Valentin <romeov@stanford.edu>
2023-04-27 21:41:59 -0700

    Simplify bash script using newer executables

--- dispatch_config.sh
+++ dispatch_config.sh
@@ -3,19 +3,17 @@
 ## that returns a /List/ of configs.
 ## The dhall file is converted to yaml and then split into one file per config, which
 ## are stored in a unique directory.
-## Then a batch job is launched for each of those files.
+## Then we can forward the configs and lauch a batch job for each file, see example usage.
+
+# Example Usage:
+# `bash dispatch_config.sh configs/mod0.dhall  | xargs -n1 sbatch submit_config.sh`
+
 set -e
-shopt -s expand_aliases
-alias sbatch=echo
 
 # make a unique directory where the config yamls are stored
-TMPDIR=$(mktemp -d -p configs)
-echo $TMPDIR
-./configs/bin/dhall-to-yaml-ng --file $1 > $TMPDIR/config.yaml
+DIR=$(mktemp -d -p configs)
+./configs/bin/dhall-to-yaml-ng --documents --file $1 > $DIR/config.yaml
+./configs/bin/yq --no-doc --split-exp '"run_"+$index' $DIR/config.yaml
+mv run*.yml $DIR
 
-# split the one yaml into a set of individual config files, and submit them as jobs!
-LEN=$(./configs/bin/yq 'length' $TMPDIR/config.yaml)
-for i in $(seq 0 $(($LEN - 1))); do
-    ./configs/bin/yq ".[$i]" $TMPDIR/config.yaml > $TMPDIR/"run$i.yaml"
-    sbatch submit_config.sh $TMPDIR/"run$i.yaml"
-done
+ls -1 $DIR/run* | cat
