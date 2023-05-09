9bebfd4e020183d0dc5cdb9edb17cdf35015880a
Romeo Valentin <romeov@stanford.edu>
2023-05-09 12:55:01 -0700

    bsub -> sbatch

--- dispatch_adversarial_sweep.sh
+++ dispatch_adversarial_sweep.sh
@@ -6,17 +6,17 @@ TMPD=$(mktemp -d)
 for DEFAULT_CONFIG in "${DEFAULT_CONFIGS[@]}"; do
     for iter in {1..3}; do
         ./configs/bin/yq e '.adversarial_training_method = "PGD", .adversarial_training_eps = "3/255", .algorithm = "adversarial_sweep"' configs/$DEFAULT_CONFIG > $TMPD/$DEFAULT_CONFIG
-        "bsub submit_config.sh $TMPD/$DEFAULT_CONFIG"
+        sbatch submit_config.sh $TMPD/$DEFAULT_CONFIG
 
         ./configs/bin/yq e '.adversarial_training_eps = "1/255"' $TMPD/$DEFAULT_CONFIG > $TMPD/${DEFAULT_CONFIG%.*}_1.$iter.yml
-        bsub submit_config.sh $TMPD/${DEFAULT_CONFIG%.*}_1.$iter.yml
+        sbatch submit_config.sh $TMPD/${DEFAULT_CONFIG%.*}_1.$iter.yml
         ./configs/bin/yq e '.adversarial_training_eps = "8/255"' $TMPD/$DEFAULT_CONFIG > $TMPD/${DEFAULT_CONFIG%.*}_2.$iter.yml
-        bsub submit_config.sh $TMPD/${DEFAULT_CONFIG%.*}_2.$iter.yml
+        sbatch submit_config.sh $TMPD/${DEFAULT_CONFIG%.*}_2.$iter.yml
 
 
         ./configs/bin/yq e '.adversarial_training_method = "FGSM"' $TMPD/$DEFAULT_CONFIG > $TMPD/${DEFAULT_CONFIG%.*}_3.$iter.yml
-        bsub submit_config.sh $TMPD/${DEFAULT_CONFIG%.*}_3.$iter.yml
+        sbatch submit_config.sh $TMPD/${DEFAULT_CONFIG%.*}_3.$iter.yml
         ./configs/bin/yq e '.adversarial_training_method = "AutoAttack"' $TMPD/$DEFAULT_CONFIG > $TMPD/${DEFAULT_CONFIG%.*}_4.$iter.yml
-        bsub submit_config.sh $TMPD/${DEFAULT_CONFIG%.*}_4.$iter.yml
+        sbatch submit_config.sh $TMPD/${DEFAULT_CONFIG%.*}_4.$iter.yml
     done
 done
