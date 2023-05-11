503ce23dd60d87a7cdfdc55536b10ca835a72bb1
Romeo Valentin <romeov@stanford.edu>
2023-05-11 13:24:04 -0700

    Fix yq piping issue

--- dispatch_adversarial_sweep.sh
+++ dispatch_adversarial_sweep.sh
@@ -5,12 +5,13 @@ TMPD="tmp"
 
 for iter in {1..1}; do
     for DEFAULT_CONFIG in "${DEFAULT_CONFIGS[@]}"; do
-        ./configs/bin/yq eval '.save_folder = "/scratch/users/romeov/holistic-reliability-evaluation/results",
-                               .data_dir = "/scratch/users/romeov/holistic-reliability-evaluation/data",
-                               .adversarial_training_method = "PGD",
-                               .adversarial_training_eps = "3/255",
-                               .algorithm = "adversarial_sweep",
-                               .max_num_workers = 16' configs/$DEFAULT_CONFIG > $TMPD/$DEFAULT_CONFIG
+        ./configs/bin/yq eval '.save_folder = "/scratch/users/romeov/holistic-reliability-evaluation/results" |
+                               .data_dir = "/scratch/users/romeov/holistic-reliability-evaluation/data" |
+                               .max_num_workers = 16 |
+                               .adversarial_training_method = "PGD" |
+                               .adversarial_training_eps = "3/255" |
+                               .algorithm = "adversarial_sweep"'\
+                                configs/$DEFAULT_CONFIG > $TMPD/$DEFAULT_CONFIG
         sbatch submit_config.sh $TMPD/$DEFAULT_CONFIG
 
         ./configs/bin/yq e '.adversarial_training_eps = "1/255"' $TMPD/$DEFAULT_CONFIG > $TMPD/${DEFAULT_CONFIG%.*}_1.$iter.yml
